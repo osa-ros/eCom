139b8fb342e479f2239e8e7528a60e036e0d45ae
osaros <yassup576@gmail.com>
2026-05-18 16:53:51 +0100

    edit src from .. to @ and with tsConfigPaths and the name of this operation is aliases paths

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 import styles from "./styles.module.css";
-import { Header, Footer } from "../../components/common";
+import { Header, Footer } from "@components/common";
 
 const { container, wrapper } = styles;
 function MainLayout() {
