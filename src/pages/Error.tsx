f52768911733487ad28e8b28e27536fb1104ea44
osaros <yassup576@gmail.com>
2026-05-31 14:04:23 +0100

    costume Error page and filter products with loader without send to the server

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,15 +1,28 @@
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-function Error() {
+import { Link, useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+const Error = () => {
+  const error = useRouteError();
+  let errorStatus: number;
+  let errorStatusText: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorStatus = error.status;
+    errorStatusText = error.statusText;
+  } else {
+    errorStatus = 404;
+    errorStatusText = "Page Not Found";
+  }
+
   return (
     <Container className="not-found">
-      <h1>404</h1>
-      <p>Page Not Found</p>
+      <h1>{errorStatus}</h1>
+      <p>{errorStatusText}</p>
       <Link to="/" replace={true}>
         Sorry, the page you are looking for doesn't exist or has been moved.
       </Link>
     </Container>
   );
-}
+};
 
 export default Error;
